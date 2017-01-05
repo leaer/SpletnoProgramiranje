89aecf42c710f7675d41bc07241b02f55b912cd5
leaer <lea.erzar@gmail.com>
2017-01-05 17:15:41 +0100

    Add files via upload

--- docs/django toDo/mysite/toDo/admin.py
+++ docs/django toDo/mysite/toDo/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Uporabnik, Opravilo
 
-# Register your models here.
+admin.site.register(Uporabnik)
+admin.site.register(Opravilo)
--- docs/django toDo/mysite/toDo/models.py
+++ docs/django toDo/mysite/toDo/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+PRIORITETA = (
+	(1, 'Najmanj pomembno'),
+	(2, 'Pomembno'),
+	(3, 'Zelo pomembno'),
+)
+
+PODROCJE = (
+	(1, 'Šola'),
+	(2, 'Delo'),
+	(3, 'Dom'),
+	(4, 'Nakupovanje'),
+	(5, 'Ostalo')
+)
+
+
+class Uporabnik(models.Model):
+	ime = models.CharField(max_length=30)
+	priimek = models.CharField(max_length=30)
+	uporabniskoIme=models.CharField(max_length=20)
+	geslo=models.CharField(max_length=30)
+	zadnjaPrijava=models.DateTimeField()
+	pass
+	
+	def __str__(self):
+		return self.uporabniskoIme        
+
+class Opravilo(models.Model):
+	uporabnik = models.ForeignKey(Uporabnik, on_delete=models.CASCADE)
+	ime = models.CharField(max_length=200)
+	createDate = models.DateTimeField(default=datetime.datetime.now)
+	dueDate = models.DateTimeField(default=datetime.datetime.now)
+	prioriteta = models.CharField(max_length=20,choices=PRIORITETA, default=1)
+	podrocje = models.CharField(max_length=20, choices=PODROCJE, default=1)
+	zapiski = models.CharField(max_length=400)
+	opravljen = models.BooleanField(default=False)
+	
+	
+
+	
